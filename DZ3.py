01474e5b4c3284f3d395bf9dc65ed478ef5c338e
Yaroslav <yaroslav.belovsky@gmail.com>
2026-02-01 21:08:29 +0200

    домашня робота 3

---
+++ DZ3.py
@@ -0,0 +1,10 @@
+from turtle import *
+
+jngl = "ttttuuuurrrrttttlllleeee"
+turtle = "" + jngl[3:5] + jngl[8] + jngl[12] + jngl[16] + jngl[20]
+print(turtle)
+shape("turtle")
+color("red", "red")
+left(9999)
+forward(500)
+print("знайшов!")
